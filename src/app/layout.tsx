0e3528a28f1805033586ca10ad495d5541bb803d
Zandewu <arifzaini483@gmail.com>
2025-07-05 13:28:12 +0700

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from 'react';
+import './globals.css';
+import Navbar from '@/components/navbar';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+import HomeComponent from "@/components/home";
+import About from "@/components/about";
+
+export default function Home() {
+  return (
+    <>
+      <HomeComponent />
+      <About />
+    </>
+  );
+}
